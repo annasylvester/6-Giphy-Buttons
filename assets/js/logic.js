e4be5e45b113262d06785279682112be3c3505d9
annasylvester <annaleesylvester@gmail.com>
2019-01-06 08:34:15 -0500

    Debugged JS

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,66 +1,68 @@
-let interests = [];
+let interests = ["Grumpy Cat", "Kermit Sips Tea", "Honey Badger", "Spongebob"];
 
-//////////// SEARCH GIPHY ////////////////
-function searchGiphy(value) {
-    const endpoint = 'https://api.giphy.com/v1/gifs/search?';
-    const params = 'api_key=i2UzHrAawKZQ8if08gTKifooFMPnXXCD&q=' + value + '&limit=10';
-    const url = endpoint + params;
-    $.ajax(url)
-        .then(handleSuccess)
-        .catch(handleError);
+
+// Display Gifs function
+function handleSuccess() {
+    $(".gifs").empty();
+
+    var search = $(this).attr("data-name");
+    console.log(search);
+
+    var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=i2UzHrAawKZQ8if08gTKifooFMPnXXCD&q=' + search + '&limit=10';
+
+    console.log(queryURL);
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function (response) {
+        var gifs = response.data;
+        console.log(gifs);
+        for (var i = 0; i < gifs.length; i++) {
+
+            var showDiv = $("<div class='col-md-4'>");
+            var rating = gifs[i].rating;
+            var defaultAnimatedSrc = gifs[i].images.fixed_height.url;
+            var staticSrc = gifs[i].images.fixed_height_still.url;
+            var showImage = $("<img>");
+            var p = $("<p>").text("Rating: " + rating);
+
+            showImage.attr("src", staticSrc);
+            showImage.addClass("giphyGif");
+            showImage.attr("data-state", "still");
+            showImage.attr("data-still", staticSrc);
+            showImage.attr("data-animate", defaultAnimatedSrc);
+            showDiv.append(p);
+            showDiv.append(showImage);
+            $(".gifs").prepend(showDiv);
+        }
+
+
+    });
 }
 
-
-//////////// DISPLAY GIFS FUNCTION //////////////
-function handleSuccess(data) {
-
-    const gifs = data.data;
-
-    for (var i = 0; i < gifs.length; i++) {
-
-        var showDiv = $("<div class='col-md-4'>");
-        var rating = gifs[i].rating;
-        var defaultAnimatedSrc = gifs[i].images.fixed_height.url;
-        var staticSrc = gifs[i].images.fixed_height_still.url;
-        var showImage = $("<img>");
-        var p = $("<p>").text("Rating: " + rating);
-
-        showImage.attr("src", staticSrc);
-        showImage.addClass("netflixGiphy");
-        showImage.attr("data-state", "still");
-        showImage.attr("data-still", staticSrc);
-        showImage.attr("data-animate", defaultAnimatedSrc);
-        showDiv.append(p);
-        showDiv.append(showImage);
-        $(".gifs").prepend(showDiv);
-    }
-
-    console.log('Data: ', gifs);
-}
-
-//////// WHEN ERROR HAPPENS ///////////////
+// When an Error happens
 function handleError(error) {
     $('.gifs').html('Oops! Something went wrong.')
-}
+};
 
 
 
-///////// WHEN SUBMIT IS CLICKED //////////////
+// When Submit is clicked
 $('#submitBtn').on("click", function (event) {
     event.preventDefault();
 
-    const newInterest = $('input[name="search"]').val().trim();
+    const newInterest = $('#input').val().trim();
     interests.push(newInterest);
     console.log(interests)
-    searchGiphy(newInterest);
 
-    $('input[name="search"]').val('');
+    $('#input').val('');
     renderButtons();
 
 });
 
 
-///////// CREATE BUTTONS ////////////////
+// Creates Buttons
 function renderButtons() {
     $("#button-bar").empty();
 
@@ -72,6 +74,22 @@ function renderButtons() {
         $("#button-bar").append(b);
     }
 }
-///////// WHEN BUTTON CLICKED ///////////////
+// When a button is clicked
+$(document).on("click", ".interest", handleSuccess);
 
+// Pause and play Gifs
+$(document).on("click", ".giphyGif", pausePlayGifs);
 
+function pausePlayGifs() {
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+}
+
+// Show original buttons
+renderButtons();
